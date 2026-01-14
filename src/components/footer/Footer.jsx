4fafea2b8a35d2fa494599b229ab2634ed9f4bdc
alexaniatoma <alexaniatoma@hotmail.com>
2026-01-14 11:45:17 -0300

    style: adicionando estilos na tag ancora

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,35 +11,35 @@ const Footer = () => {
     <div className= { s.footer }>
         <div className= { s.BoxFooter }> 
             <section>
-            <div className= {s.Logo} >
-                <img src={Logo} alt= "Imagem do logo do site Médicos Voluntários" />
-                <p>Médicos & Dentistas</p>
-            </div>     
-            <p>Saúde e cuidado sem barreiras para toda comunidade</p> 
-        </section>              
-        <section> 
-            <h3>Contato</h3>
-            <div className= { s.ContatoFooter }>
-                <img src = {Email} alt= "Ícone de email" />
-                <email>contato@medico-dentita.org</email>
-            </div>
-              <div className= { s.ContatoFooter }>
-                <img src = {Telefone} alt= "Ícone de telefone" />
-                <p>(11)3000-0000</p>
-            </div>
-              <div className= { s.ContatoFooter }>
-                <img src = {Localizacao} alt= "Ícone de localização" />
-                <p>São Paulo, Brasil</p>
-            </div>
-        </section> 
-        <section>
-            <h3>Redes Sociais</h3>
-            <ul>
-                <li><a href="#">Facebook</a></li>
-                <li><a href="#">Instagram</a></li>
-                <li><a href="#">Linkedin</a></li>
-            </ul>
-        </section>   
+                <div className= {s.Logo} >
+                    <img src={Logo} alt= "Imagem do logo do site Médicos Voluntários" />
+                    <p>Médicos & Dentistas</p>
+                </div>     
+                <p>Saúde e cuidado sem barreiras para toda comunidade</p> 
+            </section>              
+            <section> 
+                <h3>Contato</h3>
+                <div className= { s.ContatoFooter }>
+                    <img src = {Email} alt= "Ícone de email" />
+                    <email>contato@medico-dentita.org</email>
+                </div>
+                <div className= { s.ContatoFooter }>
+                    <img src = {Telefone} alt= "Ícone de telefone" />
+                    <p>(11)3000-0000</p>
+                </div>
+                <div className= { s.ContatoFooter }>
+                    <img src = {Localizacao} alt= "Ícone de localização" />
+                    <p>São Paulo, Brasil</p>
+                </div>
+            </section> 
+            <section>
+                <h3>Redes Sociais</h3>
+                <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Linkedin</a></li>
+                </ul>
+            </section>   
         </div>        
         <section className= { s.Direitos }>
             <p>&copy; 2025 Médicos e Dentistas pela Comunidade. Todos os direitos reservados.</p>          
